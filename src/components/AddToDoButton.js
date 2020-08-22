bd1cba03ae51a6b1160cb169aa9113151239862d
guptstagram <sourabh.gupta.5095@gmail.com>
2020-08-22 15:57:11 +0530

    added task input

--- src/components/AddToDoButton.js
+++ src/components/AddToDoButton.js
@@ -2,18 +2,26 @@ import React from 'react';
 import "./AddToDoButton.css";
 
 class AddToDoButton extends React.Component{
+
+
+    addTodoButtonClicked=()=>{
+        console.log("inp");
+    }
+
+
     render(){
         return(
-            <div>
-                <a className="btn-floating btn-large waves-effect waves-light red pos-bottom" href="#modal1"><i className="material-icons">add</i></a>
-
-                <div id="modal1" className="modal bottom-sheet">
-                    <div className="modal-content">
-                    <h4>Modal Header</h4>
-                    <p>A bunch of text</p>
-                    </div>
-                    <div className="modal-footer">
-                    <a href="#!" className="modal-close waves-effect waves-green btn-flat">Agree</a>
+            <div className="add-task-bottom">
+                <div className="container">
+                    <div className="row">
+                        <div className="col s8">
+                            <input placeholder="Add To Do" id="todoInput" type="text" className="validate"/>
+                        </div>
+                        <div className="col s4 left">
+                            <div className="add-button">
+                                <div className="btn-floating btn-large waves-effect waves-light red" onClick={this.addTodoButtonClicked}><i className="material-icons">add</i></div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/components/ToDoItemComponent.js
+++ src/components/ToDoItemComponent.js
@@ -1,18 +1,23 @@
 import React from 'react';
 
 class ToDoItem extends React.Component{
+
+    deleteButtonClicked=()=>{
+        console.log("hkjk");
+    }
     render(){
-        console.log(this.props);
+        console.log(this.props.todos)
         return(
             <div className="container">
                 <div className="row">
                     <div className="s12 m8 l6">
                     <ul className="collection with-header">
                         <li className="collection-header"><h4>Things to do.....</h4></li>
-                        <li className="collection-item"><div>Alvin<a href="#!" className="secondary-content"><i className="material-icons">delete_sweep</i></a></div></li>
-                        <li className="collection-item"><div>Alvin<a href="#!" className="secondary-content"><i className="material-icons">delete_sweep</i></a></div></li>
-                        <li className="collection-item"><div>Alvin<a href="#!" className="secondary-content"><i className="material-icons">delete_sweep</i></a></div></li>
-                        <li className="collection-item"><div>Alvin<a href="#!" className="secondary-content"><i className="material-icons">delete_sweep</i></a></div></li>
+                        {
+                            this.props.todos.forEach(todo=>{
+                                <li className="collection-item"><div>Alvin<a href="#!" className="secondary-content"><i className="material-icons">delete_sweep</i></a></div></li>
+                            })
+                        }
                     </ul>
                     </div>
                 </div>
