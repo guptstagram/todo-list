2921d4682fa5aa5f9b00f56f9fd83fce25ed22c6
guptstagram <sourabh.gupta.5095@gmail.com>
2020-08-27 23:55:47 +0530

    installed redux

--- src/components/ToDoItemComponent.js
+++ src/components/ToDoItemComponent.js
@@ -5,19 +5,24 @@ class ToDoItem extends React.Component{
     deleteButtonClicked=()=>{
         console.log("hkjk");
     }
+    componentDidMount=()=>{
+        let todos=this.props.todos.forEach(todo=>{
+            <li className="collection-item"><div>Alvin<a href="#!" className="secondary-content"><i className="material-icons">delete_sweep</i></a></div></li>
+        })
+        console.log(todos);
+    }
     render(){
-        console.log(this.props.todos)
+        // let {todos}=this.props;
+        // let todos=this.props.todos.forEach(todo=>{
+        //     <li className="collection-item"><div>Alvin<a href="#!" className="secondary-content"><i className="material-icons">delete_sweep</i></a></div></li>
+        // })
+        // console.log(todos);
         return(
             <div className="container">
                 <div className="row">
                     <div className="s12 m8 l6">
                     <ul className="collection with-header">
                         <li className="collection-header"><h4>Things to do.....</h4></li>
-                        {
-                            this.props.todos.forEach(todo=>{
-                                <li className="collection-item"><div>Alvin<a href="#!" className="secondary-content"><i className="material-icons">delete_sweep</i></a></div></li>
-                            })
-                        }
                     </ul>
                     </div>
                 </div>
